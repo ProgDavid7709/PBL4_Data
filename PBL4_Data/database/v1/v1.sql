241bc4e4c45a033898b17ea3b2542bfa0227cdc9
Phan Tuấn Đạt <74718210+ProgDavid7709@users.noreply.github.com>
2025-12-18 16:51:34 +0700

    Update PBL4_Data/database/v1/v1.sql

---
+++ PBL4_Data/database/v1/v1.sql
@@ -0,0 +1,4 @@
+-- PBL4 Update: Version 1
+INSERT INTO official_hashes (hash_value, hash_type, malware_name, added_by, is_pushed, date_added, db_version) VALUES ('9071e458d9a1b55b4c07b8df9111b5ba', 'md5', 'hachimi_installer.exe', 'virustotal', 1, '2025-12-18 08:41:52', 1);
+INSERT INTO official_hashes (hash_value, hash_type, malware_name, added_by, is_pushed, date_added, db_version) VALUES ('2aa287ba9c1f0b3305a225cac89fadfde3946b89', 'sha1', 'hachimi_installer.exe', 'virustotal', 1, '2025-12-18 08:42:07', 1);
+INSERT INTO official_hashes (hash_value, hash_type, malware_name, added_by, is_pushed, date_added, db_version) VALUES ('8ac0cbfc6489b076dc96f93153068157ab59ce77c7d4bf739c567ca85ff4c915', 'sha256', 'hachimi_installer.exe', 'virustotal', 1, '2025-12-18 08:42:23', 1);
