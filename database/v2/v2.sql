b0bb33547a3090c75468aec7683e6f24c739e649
Phan Tuấn Đạt <74718210+ProgDavid7709@users.noreply.github.com>
2025-12-23 00:16:10 +0700

    Update database/v2/v2.sql

--- database/v2/v2.sql
+++ database/v2/v2.sql
@@ -1,13 +1,5 @@
 -- PBL4 Update: Version 2
 BEGIN TRANSACTION;
-
-INSERT OR IGNORE INTO sig_md5 (hash, malware_name) 
-VALUES ('9071e458d9a1b55b4c07b8df9111b5ba', 'hachimi_installer.exe');
-
-INSERT OR IGNORE INTO sig_sha1 (hash, malware_name) 
-VALUES ('2aa287ba9c1f0b3305a225cac89fadfde3946b89', 'hachimi_installer.exe');
-
-INSERT OR IGNORE INTO sig_sha256 (hash, malware_name) 
-VALUES ('8ac0cbfc6489b076dc96f93153068157ab59ce77c7d4bf739c567ca85ff4c915', 'hachimi_installer.exe');
-
+INSERT OR IGNORE INTO sig_sha1 (hash, malware_name) VALUES ('2aa287ba9c1f0b3305a225cac89fadfde3946b89', 'Unknown');
+INSERT OR IGNORE INTO sig_sha256 (hash, malware_name) VALUES ('8ac0cbfc6489b076dc96f93153068157ab59ce77c7d4bf739c567ca85ff4c915', 'Unknown');
 COMMIT;
